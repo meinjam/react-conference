636ae99873c48ce8691a065173512400aaf3a7cf
meinjam <injam.cse@gmail.com>
2024-03-11 18:01:40 +0600

    accordion added and drag issue fix

--- src/components/conference/DraggableSidebar.Component.tsx
+++ src/components/conference/DraggableSidebar.Component.tsx
@@ -1,4 +1,4 @@
-import React, { Key } from 'react';
+import React from 'react';
 import { cn } from '@/lib/cn/cn';
 import Image from 'next/image';
 import UpDownIcon from '@/assets/img/up-down.svg';
@@ -12,22 +12,59 @@ interface ComponentTypes {
   setConferenceTags: React.Dispatch<React.SetStateAction<ConferenceTagType[]>>;
 }
 
+const sortableOptions = {
+  animation: 150,
+  fallbackOnBody: true,
+  swapThreshold: 0.65,
+  ghostClass: 'ghost',
+  group: 'shared',
+  forceFallback: true,
+};
+
 const DraggableSidebar = ({ activeItem, setActiveItem, conferenceTags, setConferenceTags }: ComponentTypes) => {
   return (
+    // <ReactSortable
+    //   animation={200}
+    //   delay={2}
+    //   tag={'ul'}
+    //   className='space-y-6 md:space-y-5 lg:space-y-7 xl:space-y-8'
+    //   list={conferenceTags}
+    //   setList={setConferenceTags}
+    // >
+    //   {conferenceTags?.map((tag: ConferenceTagType, i: Key) => (
+    //     <li
+    //       onClick={() => setActiveItem(tag)}
+    //       key={tag?.id}
+    //       className={cn(
+    //         'text-heading-3 cursor-pointer rounded-lg border border-[#d9d9d980] flex items-center gap-10 md:gap-4 lg:gap-9 xl:gap-11 p-2 duration-500',
+    //         activeItem === tag ? 'bg-theme-yellow border-theme-yellow shadow-lg' : 'bg-[#feffff]'
+    //       )}
+    //     >
+    //       <div className='bg-white p-2 md:p-3 xl:px-4 xl:py-[17px] rounded-lg'>
+    //         <div className='relative w-[22px] h-5 xl:w-[25px] xl:h-[22px]'>
+    //           <Image src={UpDownIcon} fill alt='sort icon' />
+    //         </div>
+    //       </div>
+    //       <span className={cn('font-bold  duration-500', activeItem === tag ? 'text-white' : 'text-black')}>
+    //         {tag?.name}
+    //       </span>
+    //     </li>
+    //   ))}
+    // </ReactSortable>
+
     <ReactSortable
-      animation={200}
-      delay={2}
       tag={'ul'}
       className='space-y-6 md:space-y-5 lg:space-y-7 xl:space-y-8'
       list={conferenceTags}
       setList={setConferenceTags}
+      {...sortableOptions}
     >
-      {conferenceTags?.map((tag: ConferenceTagType, i: Key) => (
+      {conferenceTags.map((tag: ConferenceTagType, i: number) => (
         <li
+          key={tag.id}
           onClick={() => setActiveItem(tag)}
-          key={tag?.id}
           className={cn(
-            'text-heading-3 cursor-pointer rounded-lg border border-[#d9d9d980] flex items-center gap-10 md:gap-4 lg:gap-9 xl:gap-11 p-2 duration-500',
+            'text-heading-3 cursor-pointer rounded-lg border border-[#d9d9d980] flex items-center gap-10 md:gap-4 lg:gap-9 xl:gap-11 p-2',
             activeItem === tag ? 'bg-theme-yellow border-theme-yellow shadow-lg' : 'bg-[#feffff]'
           )}
         >
@@ -36,9 +73,7 @@ const DraggableSidebar = ({ activeItem, setActiveItem, conferenceTags, setConfer
               <Image src={UpDownIcon} fill alt='sort icon' />
             </div>
           </div>
-          <span className={cn('font-bold  duration-500', activeItem === tag ? 'text-white' : 'text-black')}>
-            {tag?.name}
-          </span>
+          <span className={cn('font-bold', activeItem === tag ? 'text-white' : 'text-black')}>{tag?.name}</span>
         </li>
       ))}
     </ReactSortable>
