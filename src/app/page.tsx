aea394076cde547571f23ecfc4d26291ba3b3d2e
meinjam <injam.cse@gmail.com>
2024-03-12 22:36:00 +0600

    lazy loading added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
+import { Suspense } from 'react';
 import Conference from '@/components/homepage/Conference.Component';
 import Showcase from '@/components/homepage/Showcase.Component';
 import Sponsor from '@/components/homepage/Sponsor.Component';
+import Loading from '@/components/loading/Loading.Component';
 
 export default async function Home() {
   return (
     <>
       <Showcase />
-      <Conference />
-      <Sponsor />
+      <Suspense fallback={<Loading />}>
+        <Conference />
+      </Suspense>
+      <Suspense fallback={<Loading />}>
+        <Sponsor />
+      </Suspense>
     </>
   );
 }
